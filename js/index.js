38d67bd9703b4db1cb91feff0c36425f29f1f40c
akmalays <akmalays@gmail.com>
2026-08-14 19:47:09 +0700

    feat(navbar): setup init file section and create navbar components

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 // logic section
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  const btnMenu = document.getElementById("js-btn-menu");
+  const globalNav = document.getElementById("js-global-nav");
+
+  if (btnMenu && globalNav) {
+    btnMenu.addEventListener("click", function () {
+      const isOpen = globalNav.classList.toggle("is-open");
+      btnMenu.setAttribute("aria-expanded", isOpen ? "true" : "false");
+    });
+  }
+});
